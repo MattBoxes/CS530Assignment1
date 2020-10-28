7b496e81f908e96dad98ce4e50c4c73c37a267f4
Mathew Punsalan <mpunsalan@sdsu.edu>
2020-10-27 20:12:04 -0700

    added file opener checkers

--- disassembler.cpp
+++ disassembler.cpp
@@ -3,18 +3,47 @@
 // Mathew Punsalan 
 
 #include "disassembler.h"
+#include "OpCode.h"
 using namespace std;
 
 // need opcode class to store data structure provided
 // store opcodes into data structure
 int main(int argc, char const *argv[])
 {
-    if(argc != 2){
-        cout << "Incorrect Usage: Need two  file arguments " << endl;
+    if(argc != 2 ){
+        cout << "Incorrect Usage: Need two file " << endl;
         cout << "Correct Usage: dissem <filename>.obj <filename>.sys" << endl;
         exit(1);
     }
     cout << "two files successfully loaded in!" << endl;
+    string inFile = argv[1];
+    int extPosition = inFile.find_last_of(".");
+    if(inFile.substr(extPosition+1) != "obj") {
+        cout << "Wrong file extension!" << endl;
+        exit(1);
+    }
+    ifstream objFile(argv[1], ios::in);
+    if(!objFile) {
+        cout << ".obj File not found" << endl;
+        exit(1);
+    }
+    if(objFile.peek() == istream::traits_type::eof()) {
+        cout <<"File "<< inFile <<" is empty" << endl;
+        exit(1);
+    }
+    string fileName = inFile.substr(0,extPosition);
+    ifstream symFile((fileName + ".sym").c_str(), ios::in);
+    if(!symFile) {
+        cout << "Sym File not found" << endl;
+        exit(1);
+    }
+    if(symFile.peek() == istream::traits_type::eof()) {
+        cout << "File is empty!" << endl;
+        exit(1);
+    }
+    ofstream outFile((fileName + ".sic").c_str());
+   
+   
     //TODO:
     // open files and check for correct format using ifstream, exit if not
     // write file contents to vector data structure
